034b36dca4ab2ed3e9c6d3146fdf32d7429cf818
Roman Granacher <roman.granacher@gmail.com>
2020-04-10 17:29:38 +0200

    Basic setup for windows for windows complete works with conan

--- conanfile.py
+++ conanfile.py
@@ -1,20 +1,20 @@
 from conans import ConanFile, CMake
 
-class StringChallenge(ConanFile):
-    name = 'Challenge_Template'
+class JustAnotherInterviewTask(ConanFile):
+    name = 'JustAnotherInterviewTask'
     version = '0.1'
     settings = 'os', 'arch', 'compiler', 'build_type'
-    requires = "gtest/1.8.1@bincrafters/stable", "benchmark/1.5.0"
-    generators = "cmake_find_package" # can be updated to cmake_find_package_multi if necessary
+    requires = "qt/5.14.2@bincrafters/stable", "Catch2/2.11.1@catchorg/stable"
+    generators = "cmake_find_package_multi"
     exports = ["LICENSE.md"]
     exports_sources = ["src/*","CMakeLists.txt"]
 
     def imports(self):
-        self.copy("*.dll", "bin", "bin")
-        self.copy("*.dylib", "bin", "lib")
+        self.copy("*.dll", "", "bin")
+        self.copy("*.dylib", "", "lib")
 
     def build(self):
         cmake = CMake(self, generator='Ninja')
         cmake.configure()
         cmake.build()
-        cmake.test() # If you dont have any tests this will fail!
+        #cmake.test() # If you dont have any tests this will fail!
