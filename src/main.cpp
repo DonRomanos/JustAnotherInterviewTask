6c15fbcc2b3968b2290ae918ad3212d1d9083931
Roman Granacher <roman.granacher@gmail.com>
2020-04-10 20:27:51 +0200

    Added test setup and first tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <QApplication>
 #include <QPushButton>
 
+#include <algorithm>
+#include <iterator>
+#include <string_view>
+
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
+    if ( auto end = argv + argc; end != std::find_if(argv, end, [](std::string_view argument) {return argument == "--dryrun"; }))
+    {
+        app.quit();
+    }
 
     QPushButton button("Hello world !");
     button.show();
