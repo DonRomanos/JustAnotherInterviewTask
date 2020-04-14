d45765b83ea120395cb180ae5b4c859815dc84fb
Roman Granacher <roman.granacher@gmail.com>
2020-04-15 01:39:26 +0200

    Added ImageProvider and Tests for it

--- src/core.cpp
+++ src/core.cpp
@@ -1,5 +1,6 @@
 #include "core.hpp"
 
+#include <QImageReader>
 #include <algorithm>
 
 namespace fs = std::filesystem;
@@ -45,6 +46,70 @@ std::future<QImage> core::apply_mirror(const QImage& image, MirrorModes mirror_m
 	return std::async(std::launch::async, [image, mirror_mode]()
 		{
 			return mirror_image(image, mirror_mode);
+		}
+	);
+}
+
+namespace
+{
+	bool has_supported_extension(const fs::path& file)
+	{
+		const auto formats = QImageReader::supportedImageFormats();
+		return formats.end() != std::find_if(formats.begin(), formats.end(), [file](QByteArray extension) {return "." + extension.toStdString() == file.extension().string(); });
+	}
+
+	void advance_until_supported_image(fs::directory_iterator& current_entry)
+	{
+		while (current_entry != fs::directory_iterator() && !has_supported_extension(*current_entry))
+		{
+			++current_entry;
+		}
+	}
+}
+
+core::ImageProvider::ImageProvider(const std::filesystem::path& dir) 
+	: directory{dir}
+	, next_entry{directory}
+{
+}
+
+std::future<QImage> core::ImageProvider::load_next_image(MirrorModes mirror_mode)
+{
+	// finding the next file is done synchronously
+	advance_until_supported_image(next_entry);
+	if (next_entry == fs::directory_iterator())
+	{
+		next_entry = fs::directory_iterator(directory); // wrap around behavior if we reach the end we start from beginning
+		advance_until_supported_image(next_entry);
+		if (next_entry == fs::directory_iterator())
+		{
+			return std::async([](){return QImage(); }); // no files available for loading.
+		}
+	}
+
+	auto file_to_load = *next_entry; // There is no postfix for directory_iterator
+	++next_entry;
+
+	return std::async(std::launch::async, [file_to_load, mirror_mode]()
+		{
+			auto result = QImage(toQString(file_to_load.path().c_str()));
+			if (!result.isNull())
+			{
+				return mirror_image(result, mirror_mode);
+			}
+			// Unable to load image...
+			return QImage();
+		}
+	);
+}
+
+std::future<QImage> core::ImageProvider::apply_mirror_mode(MirrorModes new_mirror_mode, ImageInfo info)
+{
+	// first undo old mode then apply the new mode
+	return std::async(std::launch::async, [info, new_mirror_mode]()
+		{
+			QImage result = mirror_image(info.image, info.mirror_mode);
+			return mirror_image(result, new_mirror_mode);
 		}
 	);
 }
--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -5,6 +5,81 @@
 
 namespace fs = std::filesystem;
 
+TEST_CASE("Test if ImageProvider loads images as expected", "[ImageProvider]")
+{
+	core::ImageProvider to_be_tested{ fs::current_path() };
+
+	auto first_image = to_be_tested.load_next_image();
+	auto second_image = to_be_tested.load_next_image().get();
+	auto third_image = to_be_tested.load_next_image().get();
+
+	// Good for debugging, since the tests depend on the order of files the directory iterator finds.
+	std::vector<fs::path> order_of_files;
+	std::transform(fs::directory_iterator(fs::current_path()), fs::directory_iterator(), std::back_inserter(order_of_files), [](const fs::directory_entry& entry) {return entry.path(); });
+
+	SECTION("loading of an invalid image (first file) returns Null image")
+	{
+		REQUIRE(first_image.get().isNull());
+	}
+
+	SECTION("loading of a .bmp image (second file) works")
+	{
+		REQUIRE(!second_image.isNull());
+	}
+
+	SECTION("loading of a .jpg image (third file) works")
+	{
+		REQUIRE(!third_image.isNull());
+	}
+
+	SECTION("after reaching the end of the folder we start from the beginning again")
+	{
+		auto should_be_same_as_first = to_be_tested.load_next_image().get();
+		REQUIRE(first_image.get() == should_be_same_as_first);
+	}
+
+	SECTION("empty folder does not cause an infinite loop")
+	{
+		core::ImageProvider empty_provider(fs::current_path().parent_path());
+		REQUIRE(empty_provider.load_next_image().get().isNull());
+	}
+
+	SECTION("Mirror is applied when loading an image")
+	{
+		core::ImageProvider another{ fs::current_path()};
+
+		auto first_mirrored = another.load_next_image(core::MirrorModes::Horizontal);
+		auto second_mirrored = another.load_next_image(core::MirrorModes::Horizontal).get();
+		auto third_mirrored = another.load_next_image(core::MirrorModes::Vertical).get();
+
+		REQUIRE(!third_mirrored.isNull());
+		REQUIRE(third_mirrored != third_image);
+		REQUIRE(!second_mirrored.isNull());
+		REQUIRE(second_mirrored != second_image);
+
+		SECTION("Mirroring twice should return the original")
+		{
+			auto second_twice_mirrored = core::ImageProvider::apply_mirror_mode(
+				core::MirrorModes::Horizontal, core::ImageInfo{ second_mirrored }).get();
+			REQUIRE(second_twice_mirrored == second_image);
+			auto third_twice_mirrored = core::ImageProvider::apply_mirror_mode(
+				core::MirrorModes::Vertical, core::ImageInfo{ third_mirrored }).get();
+			REQUIRE(third_twice_mirrored == third_image);
+		}
+
+		SECTION("Reapplying a mirror should first undo the old mirroring")
+		{
+			auto reapplying_mirror_on_second = core::ImageProvider::apply_mirror_mode(
+				core::MirrorModes::Horizontal, core::ImageInfo{ second_mirrored, core::MirrorModes::Horizontal }).get();
+			REQUIRE(reapplying_mirror_on_second == second_mirrored);
+
+			auto reapplying_mirror_on_third = core::ImageProvider::apply_mirror_mode(
+				core::MirrorModes::Vertical, core::ImageInfo{ third_mirrored, core::MirrorModes::Vertical }).get();
+			REQUIRE(reapplying_mirror_on_third == third_mirrored);
+		}
+	}
+}
+
 TEST_CASE("Set of tests for loading an image and applying a mirror", "[start_loading_image]")
 {
 	SECTION("loading of .bmp images works")
@@ -45,6 +120,28 @@ TEST_CASE("Set of tests for loading an image and applying a mirror", "[start_loa
 		REQUIRE(unmirrored_image.cacheKey() != horizontally_mirrored.cacheKey());
 		REQUIRE(unmirrored_image.cacheKey() != vertically_mirrored.cacheKey());
 		REQUIRE(vertically_mirrored.cacheKey() != horizontally_mirrored.cacheKey());
+	}
+}
+
+TEST_CASE("Tests to see if mirroring images works as expected", "[ImageProvider]")
+{
+	auto original = core::start_loading_image(fs::current_path() / "a_real_bmp_file.bmp").get();
+	SECTION("applying horizontal mirror twice returns original")
+	{
+		auto horizontally_mirrored = core::apply_mirror(original.copy(), core::MirrorModes::Horizontal).get();
+		REQUIRE(horizontally_mirrored.cacheKey() != original.cacheKey());
+
+		auto twice_mirrored = core::apply_mirror(std::move(horizontally_mirrored), core::MirrorModes::Horizontal).get();
+		REQUIRE(twice_mirrored == original);
+	}
+
+	SECTION("applying vertical mirror twice returns original")
+	{
+		auto horizontally_mirrored = core::apply_mirror(original.copy(), core::MirrorModes::Vertical).get();
+		REQUIRE(horizontally_mirrored.cacheKey() != original.cacheKey());
+
+		auto twice_mirrored = core::apply_mirror(std::move(horizontally_mirrored), core::MirrorModes::Vertical).get();
+		REQUIRE(twice_mirrored == original);
 	}
 }
 
